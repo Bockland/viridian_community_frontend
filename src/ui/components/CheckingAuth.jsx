5a36c679b4c4fe57af7c4fdd2136451f3671d1b2
Javier Peralta <javier.peralta.ext@uss.cl>
2023-08-27 23:10:24 -0400

    fix lottie

--- src/ui/components/CheckingAuth.jsx
+++ src/ui/components/CheckingAuth.jsx
@@ -1,14 +1,7 @@
-import Lottie from "lottie-react";
 import LoadingCarga from "../img/loadingCarga.json";
 
 export const CheckingAuth = () => {
   return (
-    <Lottie 
-      animationData={LoadingCarga} 
-      style={{
-        maxWidth: '50%',
-        margin: 'auto'        
-      }}
-    />
+    <h1>cargando</h1>
   )
 }
